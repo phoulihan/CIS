52fb79ddc9278f25b2710d4b226565d8fa3ad1b6
Patrick Houlihan <pathouli@publicisgroupe.net>
2019-04-13 21:19:56 -0400

    fix

--- class_3.py
+++ class_3.py
@@ -5,6 +5,7 @@ Created on Sat Apr 13 20:30:42 2019
 @author: pathouli
 """
 import re, os
+import pandas as pd
 
 def tokenize_text(file_path):
     #read text from file and TOKENIZE
@@ -20,15 +21,20 @@ def tokenize_text(file_path):
     return text_split_out
 
 def list_txt_files(the_path_var):
-    file_names = list()
-    for filename in os.listdir(the_path):
-        if filename.endswith(".txt"): 
-            file_names.append(os.path.join(the_path, filename))
-            continue
-        else:
-            continue
+    full_list = pd.DataFrame()
+    the_dirs = os.listdir(the_path)
+    for word in the_dirs:
+        the_path_tmp = the_path + word
+        for filename in os.listdir(the_path_tmp):
+            if filename.endswith(".txt"): 
+                file_path = the_path_tmp + '/' + filename
+                full_list = full_list.append(
+                        {'body': file_path, 'label': word}, ignore_index=True)
+                continue
+            else:
+                continue
 
-    return file_names
+    return full_list
 
 the_path = 'C:/Users/pathouli/myStuff/academia/torhea/data/'
 
