fbc74a57230c8c13da4d316b37ed5a28891816bb
Patrick Houlihan <pathouli@publicisgroupe.net>
2019-04-20 22:44:30 -0400

    grid

--- nlp_master.py
+++ nlp_master.py
@@ -25,20 +25,29 @@ my_vec, labels = framework.the_vec_func(the_files)
 
 
 the_model = RandomForestClassifier()
-# the_model = MultinomialNB()
+#the_model = MultinomialNB()
 # the_model = AdaBoostClassifier()
 # the_model = SVC(gamma='auto')
 
 
-param_grid = {"max_depth": [3, 20],
-              "max_features": [1, 3, 10],
+param_grid = {"max_depth": [20, 50],
+              "n_estimators": [10, 50],
               "bootstrap": [True, False],
               "criterion": ["gini", "entropy"]}
 
-grid_search = GridSearchCV(the_model, param_grid=param_grid, cv=5)
+#param_grid = {"alpha": [0, 1.0],
+#              "fit_prior": [True, False]}
 
-print (grid_search.best_params_)
+grid_search = GridSearchCV(the_model, param_grid=param_grid, cv=5)
+grid_search.fit(my_vec, labels)
+print (grid_search.best_score_)
+
+#optimal_params = (grid_search.best_params_)
+
 #my_cv = cross_val_score(the_model, my_vec, labels, cv=10)
+
+#opt_model = RandomForestClassifier(grid_search.best_params_)
+#opt_model.fit(my_vec, labels)
 
 #print (np.mean(my_cv))
 
