4fc674259a68d8e237ccc1003887004badbe24ce
Patrick Houlihan <pathouli@publicisgroupe.net>
2019-04-20 22:06:31 -0400

    added cv

--- nlp_master.py
+++ nlp_master.py
@@ -6,6 +6,7 @@ Created on Sat Apr 13 21:38:37 2019
 """
 
 from nlp_class import nlp_func
+from sklearn.ensemble import RandomForestClassifier
 
 the_path = 'C:/Users/pathouli/myStuff/academia/torhea/data/'
 
@@ -15,7 +16,8 @@ the_files =  framework.list_txt_files(the_path)
 
 my_vec, labels = framework.the_vec_func(the_files)
 
-
+the_model = RandomForestClassifier()
+the_model.fit(my_vec, labels)
 
 #lab_enc.inverse_transform(0)
 # print (the_vec.get_feature_names())
