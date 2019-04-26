8e2b6d346d0b7fdac74fd5d02380feff7f5389e5
ty1erzHang <49329486+ty1erzHang@users.noreply.github.com>
2019-04-26 18:51:10 -0500

    homework

--- nlp_class.py
+++ nlp_class.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+gi# -*- coding: utf-8 -*-
 """
 Created on Sat Apr 13 20:30:42 2019
 
@@ -6,6 +6,7 @@ Created on Sat Apr 13 20:30:42 2019
 """
 import re, os
 import pandas as pd
+from nltk.corpus import stopwords
 
 class nlp_func():
     
@@ -16,25 +17,45 @@ class nlp_func():
         the_text_tmp.close()
         text_split_pre = ' '.join(the_text)
         text_split_pre = text_split_pre.split()
-        text_split = [re.sub('[^A-Za-z0-9]+', '', word).lower() for word in text_split_pre]
+        text_split = [re.sub('[^A-Za-z]+', '', word).lower() for word in text_split_pre]
         
         text_split_out = re.sub(' +', ' ', ' '.join(text_split))
       
         return text_split_out
     
     def list_txt_files(self, the_path):
+        the_stopwords = set(stopwords.words('english'))
         full_list = pd.DataFrame()
+        tmp2 = self.function2(the_path)
+        the_body_tmp = [word_i for word_i in tmp2.split() if word_i not in the_stopwords]
+        the_body = ' '.join(the_body_tmp)
+        full_list = full_list.append({'label': word,'body': the_body}, ignore_index=True)
+    
+        return full_list
+    
+    def function2(self, the_path):
         the_dirs = os.listdir(the_path)
         for word in the_dirs:
             the_path_tmp = the_path + word
             for filename in os.listdir(the_path_tmp):
                 if filename.endswith(".txt"): 
                     file_path = the_path_tmp + '/' + filename
-                    full_list = full_list.append(
-                            {'label': word,
-                             'body': self.tokenize_text(file_path)}, ignore_index=True)
+                    tmp = self.tokenize_text(file_path)
                     continue
                 else:
                     continue
+        
+        return tmp
     
-        return full_list
+    def the_vec_func(self, files_in):
+        from sklearn.feature_extraction.text import CountVectorizer
+        from sklearn import preprocessing
+        
+        the_vec = CountVectorizer()
+        cnt_vec = pd.DataFrame(the_vec.fit_transform(files_in.body).toarray())
+        cnt_vec.columns = the_vec.get_feature_names()
+        
+        lab_enc = preprocessing.LabelEncoder()
+        the_labels = pd.DataFrame(lab_enc.fit_transform(files_in.label))
+        
+        return cnt_vec, the_labels
