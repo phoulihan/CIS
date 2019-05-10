1eadc360387deb5ab90ed0aa1e29fbcb34b971d6
tiuo123456 <49109233+tiuo123456@users.noreply.github.com>
2019-05-10 23:58:25 +0800

    Homework: change the predict function

--- nlp_master.py
+++ nlp_master.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 13 21:38:37 2019
-
 @author: pathouli
 """
 
 from nlp_class import nlp_func
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
 
 the_path = 'C:/Users/pathouli/myStuff/academia/torhea/data/'
 
@@ -13,4 +19,29 @@ framework = nlp_func()
 
 the_files =  framework.list_txt_files(the_path)
 
+my_vec_model, label_decoder, my_vec, labels = framework.the_vec_func(the_files)
 
+
+# my_pca = framework.pca_step(my_vec, 10)
+
+
+the_model = RandomForestClassifier()
+
+#the_model = MultinomialNB()
+# the_model = AdaBoostClassifier()
+# the_model = SVC(gamma='auto')
+
+param_grid = {"max_depth": [1, 5, 20],
+              "n_estimators": [10, 50],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+
+gridsearch_model, best, opt_params = framework.grid_search_func(param_grid, the_model, my_vec, labels)
+
+optimal_rf = RandomForestClassifier()
+framework.full_train(optimal_rf, gridsearch_model, my_vec, labels) 
+
+inputted_text=input()
+prediction, prediction_proba = framework.predict(optimal_rf, my_vec_model, label_decoder, inputted_text)
+
+# 
