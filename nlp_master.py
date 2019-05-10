71414dbf24e0eb94a6dc2ba2c13c8b17e65d792a
JulyHYQ <49108912+JulyHYQ@users.noreply.github.com>
2019-05-10 17:38:24 +0800

    Update nlp_master.py

--- nlp_master.py
+++ nlp_master.py
@@ -12,5 +12,31 @@ the_path = 'C:/Users/pathouli/myStuff/academia/torhea/data/'
 framework = nlp_func()
 
 the_files =  framework.list_txt_files(the_path)
+framework = nlp_func()
+
+the_files =  framework.list_txt_files(the_path)
+
+my_vec_model, label_decoder, my_vec, labels = framework.the_vec_func(the_files)
+
+
+# my_pca = framework.pca_step(my_vec, 10)
+
+
+the_model = RandomForestClassifier()
+
+#the_model = MultinomialNB()
+# the_model = AdaBoostClassifier()
+# the_model = SVC(gamma='auto')
+
+param_grid = {"max_depth": [1, 5, 20],
+              "n_estimators": [10, 50],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+
+gridsearch_model, best, opt_params = framework.grid_search_func(param_grid, the_model, my_vec, labels)
+
+optimal_rf = RandomForestClassifier()
+framework.full_train(optimal_rf, gridsearch_model, my_vec, labels) 
+
 
 
