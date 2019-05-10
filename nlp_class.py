20753af6f3ab09c278015de6700fdafc7a99e555
ty1erzHang <49329486+ty1erzHang@users.noreply.github.com>
2019-05-10 15:40:16 -0500

    homework 5/10

--- nlp_class.py
+++ nlp_class.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 13 20:30:42 2019
-
 @author: pathouli
 """
 import re, os
@@ -26,29 +25,25 @@ class nlp_func():
     def list_txt_files(self, the_path):
         the_stopwords = set(stopwords.words('english'))
         full_list = pd.DataFrame()
-        tmp2 = self.function2(the_path)
-        the_body_tmp = [word_i for word_i in tmp2.split() if word_i not in the_stopwords]
-        the_body = ' '.join(the_body_tmp)
-        the_dirs = os.listdir(the_path)
-        for word in the_dirs:
-            full_list = full_list.append({'label': word,'body': the_body}, ignore_index=True)
-    
-        return full_list
-    
-    def function2(self, the_path):
         the_dirs = os.listdir(the_path)
         for word in the_dirs:
             the_path_tmp = the_path + word
+            print word
             for filename in os.listdir(the_path_tmp):
                 if filename.endswith(".txt"): 
                     file_path = the_path_tmp + '/' + filename
                     tmp = self.tokenize_text(file_path)
+                    the_body_tmp = [word_i for word_i in tmp.split() if word_i not in the_stopwords]
+                    the_body = ' '.join(the_body_tmp)
+                    full_list = full_list.append(
+                            {'label': word,
+                             'body': the_body}, ignore_index=True)
                     continue
                 else:
                     continue
-        
-        return tmp
     
+        return full_list
+ 
     def the_vec_func(self, files_in):
         from sklearn.feature_extraction.text import CountVectorizer
         from sklearn import preprocessing
@@ -60,4 +55,44 @@ class nlp_func():
         lab_enc = preprocessing.LabelEncoder()
         the_labels = pd.DataFrame(lab_enc.fit_transform(files_in.label))
         
-        return cnt_vec, the_labels
+        return the_vec, lab_enc, cnt_vec, the_labels
+
+    def pca_step(self, data_in, num_comp):
+        from sklearn.decomposition import PCA
+        my_vec_model = PCA(n_components=num_comp)
+        my_vec_model.fit(data_in)
+        my_vec_pca = my_vec_model.transform(data_in)
+        
+        print ("Variance Explained: " + str(sum(my_vec_model.explained_variance_ratio_)))
+        
+        return my_vec_pca
+    
+    def grid_search_func(self, param_grid, the_mode_in, the_vec_in, the_lab_in):
+        from sklearn.model_selection import GridSearchCV
+        
+        grid_search = GridSearchCV(the_mode_in, param_grid=param_grid, cv=5)
+        best_model = grid_search.fit(the_vec_in, the_lab_in)
+        
+        max_score = grid_search.best_score_
+        best_params = grid_search.best_params_
+        
+        return best_model, max_score, best_params
+    
+    def full_train(self, the_model, gridsearch_model_in, my_vec_in, labels_in):
+        the_model.set_params(**gridsearch_model_in.best_params_)
+        the_model.fit(my_vec_in, labels_in)
+        feature_imp = the_model.feature_importances_
+        
+        return the_model, feature_imp
+    
+    def predict(self, the_model, vec_mod, label_dec, text_in):
+        needed_text_1 = ' '.join(text_in)
+        needed_text_1 = needed_text_1.split()
+        needed_text_2 = [re.sub('[^A-Za-z]+', '', word).lower() for word in needed_text_1]
+        need_text_done = re.sub(' +', ' ', ' '.join(needed_text_2))
+        test_data = vec_mod.transform([need_text_done])
+        the_prediction = label_dec.inverse_transform(the_model.predict(test_data)[0])
+        print (the_model.predict_proba(test_data)[0])
+        score = max(the_model.predict_proba(test_data)[0])
+        
+        return the_prediction, score
